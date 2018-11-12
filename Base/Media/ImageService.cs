ac7d5bbc95ebbf685faa1ccd10101cd7ad375ac2
Леонид Ершов <kindle47@outlook.com>
2018-11-12 16:32:49 +0300

    ++

--- Base/Media/ImageService.cs
+++ Base/Media/ImageService.cs
@@ -1,12 +1,21 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.Specialized;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using ImageResizer;
 
 namespace Base.Media
 {
-    class ImageService
+    public interface IImageService
     {
+    }
+
+    public class ImageService : IImageService
+    {
+        void test()
+        {
+        }
     }
 }
